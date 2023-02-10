7351429d2e09e1827b8a45bb1f5595e6abde05f2
Michael Drogalis <michael.drogalis@onyxplatform.org>
2023-02-09 19:33:13 -0800

    Fix node path.

--- emacs/init.el
+++ emacs/init.el
@@ -156,3 +156,9 @@
 
 (use-package multiple-cursors
   :ensure t)
+
+(use-package exec-path-from-shell
+  :ensure t
+  :config
+  (when (memq window-system '(mac ns x))
+    (exec-path-from-shell-initialize)))
